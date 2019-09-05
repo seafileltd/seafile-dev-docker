e7c08c919164e3808a9d9db4494b33f3b616a6d8
sniper-py <dzmbbs@qq.com>
2019-09-05 15:42:01 +0800

    update pip elasticsearch

--- image/scripts/pip_install.sh
+++ image/scripts/pip_install.sh
@@ -1,5 +1,6 @@
-pip install python-cas djangosaml2 sqlalchemy pillow elasticsearch \
-  elasticsearch-dsl pycryptodome pylibmc django-pylibmc
+pip install python-cas djangosaml2 cffi sqlalchemy pillow \
+  pycryptodome configparser pylibmc django-pylibmc \
+  elasticsearch==5.5.0 elasticsearch-dsl==5.4.0
 
 cd /tmp
 wget https://raw.githubusercontent.com/haiwen/seahub/${BRANCH}/requirements.txt
